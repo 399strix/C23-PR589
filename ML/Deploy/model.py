642e392f7753f8e07cad491d046bffcc45a74c0c
Danu Adi Nugroho <gaul.gaul5@gmail.com>
2023-06-03 21:00:35 +0700

    Added logic to handle no prediction result

--- ML/Deploy/model.py
+++ ML/Deploy/model.py
@@ -1,8 +1,6 @@
 from PIL import Image
 import tensorflow as tf
 import numpy as np
-# import base64
-# import io
 
 def load_model(path_artificial, path_natural):
     model_A = tf.keras.models.load_model(path_artificial)
@@ -23,14 +21,7 @@ def preprocess(image):
     image_arr = image.numpy()
     final_image = datagen.apply_transform(image_arr, {'theta': 0})
     final_image = np.expand_dims(final_image, axis=0)
-
-    # pil_image = Image.fromarray(np.uint8(final_image[0]))
-    # image_bytes = io.BytesIO()
-    # pil_image.save(image_bytes, format='JPEG')
-    # image_bytes = image_bytes.getvalue()
-    # image_base64 = base64.b64encode(image_bytes).decode('utf-8')
-
-    return final_image#, image_base64
+    return final_image
 
 def predict(model, image, label):
     predictions = model.predict(image)
@@ -42,8 +33,11 @@ def predict(model, image, label):
         pred_confidence.append([float(pred[i]) for i in label_indices])
 
     predictions = {}
-    for a, b in zip(pred_label, pred_confidence):
-        predictions[a[0]] = b[0]
+    if len(pred_label[0]) != 0:
+        for a, b in zip(pred_label, pred_confidence):
+            predictions[a[0]] = b[0]
+    else:
+        predictions = None
 
-    return predictions
+    return predictions, pred_label
 
