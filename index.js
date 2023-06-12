40a8fcd46bd751f036e213e6c74471d3b3d33d70
DzakirizaDF <ddf7802@gmail.com>
2023-06-12 15:27:45 +0700

    databaseenv

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { config } from 'dotenv';
 config();
 
 const app = express();
-const port = process.env.NODE_ENV || 'development';
+const port = process.env.PORT || 8080;
 
 app.get("/api",(req, res)=>{
     res.json({
