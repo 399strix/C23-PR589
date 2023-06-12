c3e31324cef9d1ff2025ac0d0810da7103f97550
DzakirizaDF <ddf7802@gmail.com>
2023-06-12 17:14:53 +0700

    databaseenv

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 import express from "express";
 import cors from "cors";
 import ProductRoute from "./routes/ProductRoutes.js";
-import { config } from 'dotenv';
+import {config} from 'dotenv';
 config();
 
 const app = express();
-const port = process.env.PORT || 8080;
+const env = process.env.NODE_ENV || 'development';
 
-app.get("/api",(req, res)=>{
-    res.json({
-        success:1,
-        message:"This is Nice One!"
-    });
-});
+if (env === 'production') {
+    dotenv.config({ path: `.env.${env}` });
+} else {
+    dotenv.config({ path: '.env' });
+}
 
 app.use(cors());
 app.use(express.json());
 app.use(ProductRoute);
 
 app.listen(port, () => {
-    console.log(`Listening: http://localhost:${port}`);
+    console.log(`Listening: http://localhost:${env}`);
 });
