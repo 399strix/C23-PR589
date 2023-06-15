4f4cbf7af7c5636099de34a9adeae15711e2d4f9
Danu Adi Nugroho <gaul.gaul5@gmail.com>
2023-06-15 13:37:34 +0700

    Changed path

--- ML/Deploy/app.py
+++ ML/Deploy/app.py
@@ -13,8 +13,8 @@ LABEL_BUATAN = {0: 'Bendungan', 1: 'Kebun Binatang', 2: 'Kolam Renang', 3: 'Muse
 # END OF V1
 
 # V2 - CHANGE THIS INSTEAD
-PATH_MODEL_BUATAN = "/h5/buatan.h5"
-PATH_MODEL_ALAM = "/h5/alam.h5"
+PATH_MODEL_BUATAN = "h5/buatan.h5"
+PATH_MODEL_ALAM = "h5/alam.h5"
 PORT = 5000
 DB_URL = "https://c23-pr589-ru5cfkck3a-uc.a.run.app/"
 # USED FOR API KEY, ENSURE THIS SAME WITH IN ANDROID APP
